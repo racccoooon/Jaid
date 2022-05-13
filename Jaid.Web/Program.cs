97d680afb8ca22337b25cd60ae430f3cdff47e61
Alex <69281817+juuuuce@users.noreply.github.com>
2022-05-13 17:27:28 +0100

    Project setup

--- Jaid.Web/Program.cs
+++ Jaid.Web/Program.cs
@@ -1,6 +1,11 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Jaid.Web;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-app.Run();
+builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
+
+await builder.Build().RunAsync();
